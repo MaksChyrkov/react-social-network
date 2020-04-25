7cdf8ad347b151f358cdfa29c58fc6e68e297189
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-04-25 23:35:15 +0300

    Started to configure the Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import css from './Dialogs.module.scss';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogs}>
+            <div className={css.dialogsItems}>
+                <div className={css.dialog + ' ' + css.active}>
+                    Maks
+                </div>
+                <div className={css.dialog}>
+                    Ilya
+                </div>
+                <div className={css.dialog}>
+                    Vova
+                </div>
+                <div className={css.dialog}>
+                    Sasha
+                </div>
+                <div className={css.dialog}>
+                    Kostya
+                </div>
+                <div className={css.dialog}>
+                    Dima
+                </div>
+            </div>
+            <div className={css.messages}>
+                <div className={css.message}>Hi</div>
+                <div className={css.message}>How are you?</div>
+                <div className={css.message}>Yo</div>
+            </div>
         </div>
     );
 }
