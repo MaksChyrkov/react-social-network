a779741074e1e72b6409639152ebab54e7b2dec9
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-04-25 13:36:04 +0300

    Starting to develop social network

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Microsoft_Edge_logo_%282019%29.svg/512px-Microsoft_Edge_logo_%282019%29.svg.png" />
+        </header>
+    );
+}
+
+export default Header;
