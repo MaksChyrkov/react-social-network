bff20b9e9f9e84601596400afeef1cd477ebe099
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-04-26 17:10:53 +0300

    Carried out all the data to index.js and used props to get it in files

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,24 +5,9 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogs = [
-        {id: 1, name: 'Maks'},
-        {id: 2, name: 'Ilya'},
-        {id: 3, name: 'Dima'},
-        {id: 4, name: 'Sasha'},
-        {id: 5, name: 'Anton'},
-        {id: 6, name: 'Kostya'}
-    ]
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
 
-    let messages = [
-        {id: 1, message: 'Hi!'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: "What a sunny weather"}
-    ]
-
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-
-    let messagesElements = messages.map(m => <Message message={m.message}/>);
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
 
     return (
         <div className={css.dialogs}>
