5297bceb429b3710f705e3a9f1cece75db4546e7
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-04-27 13:33:24 +0300

    Deleted render.js and did some manipulations in state.js and index.js to get rid of circular dependency

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,3 @@
-import {rerenderEntireTree} from "../render";
-
 let state = {
     profilePage: {
         posts: [
@@ -28,7 +26,9 @@ let state = {
     }
 }
 
-export let addPost = () => {
+let rerenderEntireTree = () => {}
+
+export const addPost = () => {
     let newPost = {
         id: 4,
         message: state.profilePage.newPostText,
@@ -39,9 +39,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
