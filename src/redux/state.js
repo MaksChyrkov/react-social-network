ba6666673f969a89d4f472be792e4539ae15f246
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-04-27 12:00:43 +0300

    Started using FLUX architecture so modified state.js and MyPosts.js for <textarea />

--- src/redux/state.js
+++ src/redux/state.js
@@ -6,7 +6,8 @@ let state = {
             {id: 1, message: "Hi, how are you?", likesCount: "15"},
             {id: 2, message: "It`s my first post", likesCount: "10"},
             {id: 3, message: "Hello react!", likesCount: "20"}
-        ]
+        ],
+        newPostText: ""
     },
 
     dialogsPage: {
@@ -27,13 +28,19 @@ let state = {
     }
 }
 
-export let addPost = (messagePost) => {
+export let addPost = () => {
     let newPost = {
-        id: 5,
-        message: messagePost,
+        id: 4,
+        message: state.profilePage.newPostText,
         likesCount: "0"
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = "";
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
