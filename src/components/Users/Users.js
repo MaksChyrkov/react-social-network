e15ec90810db88ed43d220d12927e8d73ad3cade
MaksChyrkov <46497173+MaksChyrkov@users.noreply.github.com>
2020-05-09 14:08:54 +0300

    Fixed side effect in 'Users'

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -7,14 +7,17 @@ import avatar from "../../assets/img/hotpng.com.png"
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            });
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div className={css.usersPage} key={u.id}>
                         <img src={u.imgUrl != null ? u.imgUrl : avatar}/>
